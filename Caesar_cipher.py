6131384f80852ba0d3896eb35249e6dd7be045d3
Hoshi0420 <kpcloud9@gmail.com>
2023-05-01 17:40:56 +0900

    feat: Create decipher. - Create dicipher code - print dicipher text

--- Caesar_cipher.py
+++ Caesar_cipher.py
@@ -6,9 +6,17 @@ def encipher(p, k):
         c += chr(t)
     return c
 
+def decipher(p, k):
+    c = ''
+    for i in range(len(p)):
+        a = ord(p[i])
+        t = a - k
+        c += chr(t)
+    return c
 
 Text = input("Input the word: ")
 level = int(input("암호화 단계: "))
 
-print("원문:" + Text)
-print("암호화 :" + encipher(Text, level))
+print("원문: " + Text)
+print("암호화: " + encipher(Text, level))
+print("복호화: " + decipher(encipher(Text, level), level))
